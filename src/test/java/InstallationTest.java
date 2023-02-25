de66fca732361e3d06c1daffd0cb530a50e298b6
Michal Nowakowski <michal.nowakowski85@gmail.com>
2023-02-25 23:55:55 +0100

    InstallationTest added

---
+++ src/test/java/InstallationTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstallationTest {
+
+    @Test
+    void validateDate() {
+    }
+
+    @Test
+    void validateModel() {
+    }
+
+    @Test
+    void validatePower() {
+    }
+
+    @Test
+    void validateSerialNumber() {
+    }
+}
